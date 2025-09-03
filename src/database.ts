7fa160ff4d3500ad8b7a994b5de752fcf5ddd41c
KaiokkFernandes <kaiofernandess654@gmail.com>
2025-09-02 23:17:58 -0300

    feat: adiciona suporte a módulos ES e define interfaces para usuários, pivôs e irrigação

---
+++ src/database.ts
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid';
+
+// --- INTERFACES (Definição dos Tipos) ---
+
+
+export interface User {
+  id: string;
+  username: string;
+  passwordHash: string;
+}
+
+export interface Pivot {
+  id: string;
+  description: string;
+  flowRate: number;
+  minApplicationDepth: number;
+  userId: string;
+}
+
+export interface Irrigation {
+  id: string;
+  pivotId: string;
+  applicationAmount: number;
+  irrigationDate: string;
+  userId: string;
+}
+
+
+export const users: User[] = [];
+export const pivots: Pivot[] = [];
+export const irrigations: Irrigation[] = [];
